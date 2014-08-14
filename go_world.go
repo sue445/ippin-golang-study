881540691a70f379681b6f48219451f0c5131f9e
sueyoshi_go <go.sueyoshi@drecom.co.jp>
2014-08-14 20:53:31 +0900

    P.7

--- go_world.go
+++ go_world.go
@@ -7,3 +7,7 @@ import (
 func main(){
 	fmt.Printf("Go World!\n")
 }
+
+func Double(i int) int {
+	return 2
+}
--- go_world_test.go
+++ go_world_test.go
@@ -7,4 +7,8 @@ import (
 func TestDouble(t *testing.T){
 	param := 1
 	result := Double(param)
+
+	if result != 2 {
+		t.Errorf("Double(%d) = %d, want %d", param, result, 2)
+	}
 }
